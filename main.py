c1944a2689188af1c613491dec87c548c338caf2
James-QiuHaoran <jamesqiu@hku.hk>
2017-11-04 10:26:49 +0800

    add images

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame;
-import os;
+import sys;
+
+BLOCK_SIZE = 50
+WINDOW_WIDTH = BLOCK_SIZE * 8
+WINDOW_HEIGHT = BLOCK_SIZE * 8
 
 def quitGame():
 	pygame.quit()
@@ -11,10 +15,23 @@ class Game_Engine(object):
 		# TODO
 
 	def preparation(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+		self.clock = pygame.time.Clock()
+		pygame.display.set_caption('Othello')
+
+		# set font
+		self.font = pygame.font.SysFont(None, 48)
+
+		# set images
+		self.images['board'] = pygame.image.load('images/board.png')
+        self.images['black'] = pygame.image.load('images/black.png')
+        self.images['white'] = pygame.image.load('images/white.png')
 		# TODO
 
 	def newGame(self):
 		# TODO
+		pass
 
 	def start(self):
 		self.preparation()
@@ -42,21 +59,32 @@ class Game_Engine(object):
 
 	def drawText(self, text, font, surface, x, y):
 		# TODO
+		pass
 
 	def drawBoard(self):
 		# TODO
+		pass
 
 	# Definition of Event Handlers
 	def keydownHandler(self, event):
 		# TODO
+		pass
+
 	def keyupHandler(self, event):
-		# TODO 
+		# TODO
+		pass
+
 	def mousedownHandler(self, event):
 		# TODO
+		pass
+
 	def mouseupHandler(self, event):
 		# TODO
+		pass
+
 	def mousemoveHandler(self, event):
 		# TODO
+		pass
 
 if __name__ == '__main__':
 	engine = Game_Engine()
